d697ac73c0927fbe8debf33fb0c8e9e5646d98d5
BanJung <higkdl813@gmail.com>
2024-08-10 17:42:35 +0900

    [REFACTOR] 세션 저장소로 DB 설정 (chapter 5)

--- src/main/java/com/study/springboot/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/study/springboot/config/auth/LoginUserArgumentResolver.java
@@ -20,7 +20,7 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
     public boolean supportsParameter(MethodParameter parameter) {
         // 컨트롤러 메서드의 특정 파라미터를 지원하는지 판단
         // 파라미터에 @LoginUser 어노테이션이 붙어 있고, 파라미터 클래스 타입이 SessionUser.class 인 경우 true 반환
-        boolean isLoginUserAnnotation=parameter.getMethodAnnotation(LoginUser.class)!=null;
+        boolean isLoginUserAnnotation=parameter.getParameterAnnotation(LoginUser.class)!=null;
         boolean isUserClass= SessionUser.class.equals(parameter.getParameterType());
         return isLoginUserAnnotation && isUserClass;
     }
